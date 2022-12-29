5a1da5685f8c2858c7a3e00c75dd101e6d5b054e
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2022-12-30 01:57:00 +0900

    배열최소공배수 GCD함수

--- src/Programers/Level1/배열최소공배수.java
+++ src/Programers/Level1/배열최소공배수.java
@@ -1,80 +1,28 @@
 package Programers.Level1;
 
-import java.util.*;
-
 public class 배열최소공배수 {
-    public static final int[] sosoo = { 2,3,5,7 };
+
     public int solution(int[] arr) {
         int answer = 1;
-        int cnt;
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        Map<String, Integer> result = new HashMap<String, Integer>();
-        initMap(result);
-        initMap(map);
-        for(int num : arr) {
-            map = getPrimeNumber(num, num, map);
-            cnt = 0;
-
-            for(Map.Entry<String, Integer> entry : map.entrySet()) {
-                if(result.get(entry.getKey()) == null)
-                    result.put(entry.getKey(), entry.getValue());
-                else {
-                    if(result.get(entry.getKey()) < entry.getValue())
-                        result.put(entry.getKey(), entry.getValue());
-                    if(entry.getValue() == 0)
-                        cnt++;
-                }
-
-            }
-            if(cnt == 4)
-                result.put(Integer.toString(num), 1);
-
-            initMap(map);
-        }
-        for(Map.Entry<String, Integer> entry : result.entrySet()) {
-            for(int i = 0; i < entry.getValue(); i++) {
-                answer *= Integer.parseInt(entry.getKey());
-
-            }
-
-        }
+        int gcdValue;
+        for(int i : arr)
+            answer = lcd(i, answer);
 
         return answer;
     }
 
-    private Map<String, Integer> getPrimeNumber(int num, int origin, Map<String, Integer> map) {
-        if(num == 2 || num == 3 || num == 5 || num ==7 ) {
-            map.put(Integer.toString(num),
-                    map.get(Integer.toString(num)) + 1);
-            return map;
-        }else {
-            for (int i = 2; i <= origin/2; i++) {
-                if (num % i == 0) {
-                    if(map.get(Integer.toString(i)) == null)
-                        map.put(Integer.toString(i), 1);
-                    else {
-                        map.put(Integer.toString(i),
-                                map.get(Integer.toString(i)) + 1);
-                    }
+    private int lcd(int a, int b) { return (a * b) / gcd(a, b); }
 
-                    return getPrimeNumber(num / i, num, map);
-                }
-
-            }
-
+    private int gcd(int a, int b) {
+        int tmp;
+        if(a < b) {
+            tmp = b;
+            b = a;
+            a = tmp;
         }
-
-
-        return map;
+        if(b == 0) return a;
+        return gcd(b, a%b);
     }
-
-    private Map<String, Integer> initMap(Map<String, Integer> map) {
-        map.clear();
-        map.put("2", 0); map.put("3", 0); map.put("5", 0); map.put("7", 0);
-
-        return map;
-    }
-
     public static void main(String[] args) {
         int[] case1 = {2,6,8,14};
         int[] case2 = {1, 2, 3};
