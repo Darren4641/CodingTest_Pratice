88b51f54f5482f04202df618dfc994245245aa3c
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2022-12-31 20:16:54 +0900

    음양 더하기

---
+++ src/Programers/Level1/음양더하기.java
@@ -0,0 +1,24 @@
+package Programers.Level1;
+
+public class 음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i = 0; i < absolutes.length; i++) {
+            if(signs[i])
+                answer += absolutes[i];
+            else
+                answer -= absolutes[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] x = {4,7,12};
+        boolean[] y = {true,false,true};
+        System.out.println(new 음양더하기().solution(x, y));
+
+
+    }
+}
