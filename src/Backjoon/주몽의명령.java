5b6e3b4756c9768a93f511c1ec27adc9a70fb251
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-02-17 22:59:27 +0900

    Silver 5

---
+++ src/Backjoon/주몽의명령.java
@@ -0,0 +1,54 @@
+package Backjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 주몽의명령 {
+
+    public static int solution(int[] arr, int num) {
+        Arrays.sort(arr);
+
+        int answer = 0;
+        int start = 0;
+        int end = arr.length - 1;
+        int sum = 0;
+        while(start < end){
+            sum = arr[start] + arr[end];
+            if(sum < num) {
+                start++;
+            }else if(sum > num) {
+                end--;
+            }else {
+                answer++;
+                start++;
+                end--;
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            int size = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            int[] arr = new int[size];
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int i = 0; i < size; i++) {
+
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            bw.write(String.valueOf(solution(arr, num)));
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
